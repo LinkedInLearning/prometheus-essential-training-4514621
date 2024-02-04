85a5353f846bad0d48ee814a04eec1484001888b
Opeyemi Onikute <opeyemionikute@yahoo.com>
2024-02-04 00:21:20 +0000

    add pushgateway example

--- examples/client-library/main.go
+++ examples/client-library/main.go
@@ -7,11 +7,13 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/client_golang/prometheus/push"
 )
 
 func main() {
 	port := ":8082"
 
+	// export a simple counter
 	simpleCounter := promauto.NewCounter(prometheus.CounterOpts{
 		Name: "myapp_simple_count_total",
 		Help: "The total number of simple counts",
@@ -19,6 +21,20 @@ func main() {
 
 	// Increment the counter
 	simpleCounter.Inc()
+
+	// push a simple metric to the pushgateway
+	completionTime := prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "db_backup_last_completion_timestamp_seconds",
+		Help: "The timestamp of the last successful completion of a DB backup.",
+	})
+
+	completionTime.SetToCurrentTime()
+	if err := push.New("http://localhost:9091", "db_backup").
+		Collector(completionTime).
+		Grouping("db", "customers").
+		Push(); err != nil {
+		fmt.Println("Could not push completion time to Pushgateway:", err)
+	}
 
 	fmt.Printf("listening on port %s\n", port)
 	http.Handle("/metrics", promhttp.Handler())
